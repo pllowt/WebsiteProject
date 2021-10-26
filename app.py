d20108b55f066ba55a085dcf4339fc8330aebd83
lowth01 <paul.lowther01@hotmail.com>
2021-10-26 12:46:44 +0100

    Added base.html and home.html.j2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html.j2')
 
 if __name__ == '__main__':
     app.run(debug=True)
